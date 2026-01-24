c502cc0116cc0ce108664ad8c781ea5388682d63
Magnus <s224188@dtu.dk>
2026-01-24 16:14:45 +0100

    Fix withBase helper to normalize BASE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cruesli.github.io",
+    site: "https://cruesli.github.io/recipes",
     base: "/recipes",
   });
